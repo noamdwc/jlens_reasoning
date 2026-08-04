07ce296d18a329746e3afbb58d38dd5c5560eba0
Noam Carmon <noam.carmon2@gmail.com>
2026-08-04 13:03:43 +0300

    feat: run resumable FLenQA accuracy evaluation

--- src/jlens_reasoning/benchmarks/flenqa/__init__.py
+++ src/jlens_reasoning/benchmarks/flenqa/__init__.py
@@ -1,5 +1,11 @@
 """Validated FLenQA dataset preparation and paired-lens execution."""
 
+from jlens_reasoning.benchmarks.flenqa.accuracy import (
+    AccuracyRunConfig,
+    AccuracyRunManifest,
+    load_accuracy_results,
+    run_accuracy,
+)
 from jlens_reasoning.benchmarks.flenqa.dataset import (
     FlenqaPrompt,
     FlenqaRow,
@@ -13,13 +19,17 @@ from jlens_reasoning.benchmarks.flenqa.dataset import (
 )
 
 __all__ = [
+    "AccuracyRunConfig",
+    "AccuracyRunManifest",
     "FlenqaPrompt",
     "FlenqaRow",
     "SourceProvenance",
     "build_prompt_text",
     "compute_prompt_id",
     "deduplicate",
+    "load_accuracy_results",
     "normalize_rows",
+    "run_accuracy",
     "verify_count_invariants",
     "verify_schema",
 ]
